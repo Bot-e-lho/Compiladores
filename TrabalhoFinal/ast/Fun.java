c7cdc5de8579fc916046ea50a9fa8b5066610f28
bot-e-lho <mrbotelho@inf.ufpel.edu.br>
2025-07-25 19:15:03 -0300

    adiantando modificações no bela.jj

--- TrabalhoFinal/ast/Fun.java
+++ TrabalhoFinal/ast/Fun.java
@@ -2,15 +2,15 @@ package ast;
 
 import java.util.ArrayList;
 
-public class Fun{
+public class Fun {
 	public String nome;
 	public ArrayList<ParamFormalFun> params;
 	public String retorno;
 	public ArrayList<VarDecl> vars;
 	public ArrayList<Comando> body;
-	
-	public Fun(String nome,ArrayList<ParamFormalFun> params, String retorno,ArrayList<VarDecl> vars,ArrayList<Comando> body)
-	{
+
+	public Fun(String nome, ArrayList<ParamFormalFun> params, String retorno, ArrayList<VarDecl> vars,
+			ArrayList<Comando> body) {
 		this.nome = nome;
 		this.params = params;
 		this.retorno = retorno;
--- TrabalhoFinal/ast/Prog.java
+++ TrabalhoFinal/ast/Prog.java
@@ -2,13 +2,12 @@ package ast;
 
 import java.util.ArrayList;
 
-public class Prog{
+public class Prog {
     public Main main;
     public ArrayList<Fun> fun;
-    public Prog(Main main, ArrayList<Fun> fun)
-    {
+
+    public Prog(Main main, ArrayList<Fun> fun) {
         this.main = main;
         this.fun = fun;
     }
 }
-
